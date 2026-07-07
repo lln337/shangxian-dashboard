81c0e71045704252f975adb8f3a4e280995d1622
lln337 <516357321@qq.com>
2026-07-07 10:33:34 +0800

    [正式版更新] data/password-config.js

---
+++ data/password-config.js
@@ -0,0 +1,8 @@
+// 密码验证配置 - 由 crypto_tool.py 自动生成，请勿手动修改
+window.DASHBOARD_PASSWORD_HASH = "02e6a4bef4877eaebe320f6ed5c0d579e71149156ee9345a19bce54cb106d20f";
+window.DASHBOARD_SALT = "eZibuGx68iB/o2AaEOvrPA==";
+window.DASHBOARD_PBKDF2_ITER = 600000;
+window.DASHBOARD_ENCRYPTED_FILES = [
+  "part_sort_data.json",
+  "part_plan_data.json"
+];
